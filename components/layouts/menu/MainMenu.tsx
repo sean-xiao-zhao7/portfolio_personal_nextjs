35f62a98c289de199119ef90bead249117443efe
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-01-08 12:08:00 -0500

    Add menu gaps

--- components/layouts/menu/MainMenu.tsx
+++ components/layouts/menu/MainMenu.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 
 export default function MainMenu() {
-    return <ul className="absolute p-8">
-        <li><Link href='/'>JPX</Link></li>
-        <li><Link href='/cv'>CV</Link></li>
-        <li><Link href='/ai'>AI</Link></li>
-        <li><Link href='/music'>Music</Link></li>
+    return <ul className="absolute p-8 flex flex-col gap-4">
+        <li><Link href='/' className="text-2xl font-semibold">JPX</Link></li>
+        <li><Link href='/cv' className="text-2xl font-semibold">CV</Link></li>
+        <li><Link href='/ai' className="text-2xl font-semibold">AI</Link></li>
+        <li><Link href='/music' className="text-2xl font-semibold">Music</Link></li>
     </ul>
 }
