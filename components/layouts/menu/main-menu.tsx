030bd6d5198b4e4dfddcec51dec9c536161b657f
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-01-08 16:06:30 -0500

    Put icons inside anchors in main menu

--- components/layouts/menu/main-menu.tsx
+++ components/layouts/menu/main-menu.tsx
@@ -10,10 +10,10 @@ export default function MainMenu({ setColorMode, currentColorMode }: { setColorM
     const modeSwitcherHandler = () => { setColorMode((prevState) => prevState === 'light' ? 'dark' : 'light') };
 
     return <ul className="absolute p-8 flex flex-col gap-4 items-center">
-        <li className="flex flex-col items-center"><House fill={iconFillColor} color={iconOutlineColor} size={40} /><Link href='/' className="text-2xl font-semibold">JPX</Link></li>
-        <li className="flex flex-col items-center"><FileUser fill={iconFillColor} color={iconOutlineColor} size={40} /><Link href='/cv' className="text-2xl font-semibold">CV</Link></li>
-        <li className="flex flex-col items-center"><Bot fill={iconFillColor} color={iconOutlineColor} size={40} /><Link href='/ai' className="text-2xl font-semibold">AI</Link></li>
-        <li className="flex flex-col items-center"><Music fill={iconFillColor} color={iconOutlineColor} size={40} /><Link href='/music' className="text-2xl font-semibold">Music</Link></li>
+        <li><Link href='/' className="text-2xl font-semibold flex flex-col items-center"><House fill={iconFillColor} color={iconOutlineColor} size={40} /> JPX</Link></li>
+        <li><Link href='/cv' className="text-2xl font-semibold flex flex-col items-center"><FileUser fill={iconFillColor} color={iconOutlineColor} size={40} /> CV</Link></li>
+        <li><Link href='/ai' className="text-2xl font-semibold flex flex-col items-center"><Bot fill={iconFillColor} color={iconOutlineColor} size={40} /> AI</Link></li>
+        <li><Link href='/music' className="text-2xl font-semibold flex flex-col items-center"><Music fill={iconFillColor} color={iconOutlineColor} size={40} /> Music</Link></li>
         <li><ModeSwitcher text={currentColorMode === 'dark' ? 'Light' : 'Dark'} modeSwitchHandler={modeSwitcherHandler} on={currentColorMode === 'dark'} /></li>
     </ul>
 }
