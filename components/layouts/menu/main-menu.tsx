238b4e2e97017908f44bba7415a4a99b47bd46b4
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-01-08 15:44:02 -0500

    Make main menu items flex col center

--- components/layouts/menu/main-menu.tsx
+++ components/layouts/menu/main-menu.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import { Dispatch, SetStateAction } from "react";
 
+import { Bot, Music, FileUser, House } from 'lucide-react';
+
 import ModeSwitcher from "@/components/widgets/mode-switcher";
 
 export default function MainMenu({ setColorMode, currentColorMode }: { setColorMode: Dispatch<SetStateAction<string>>, currentColorMode: string }) {
 
     const modeSwitcherHandler = () => { setColorMode((prevState) => prevState === 'light' ? 'dark' : 'light') };
 
-    return <ul className="absolute p-8 flex flex-col gap-4">
-        <li><Link href='/' className="text-2xl font-semibold">JPX</Link></li>
-        <li><Link href='/cv' className="text-2xl font-semibold">CV</Link></li>
-        <li><Link href='/ai' className="text-2xl font-semibold">AI</Link></li>
-        <li><Link href='/music' className="text-2xl font-semibold">Music</Link></li>
-        <li><ModeSwitcher text={currentColorMode === 'dark' ? 'Light' : 'Dark'} modeSwitchHandler={modeSwitcherHandler} /></li>
+    return <ul className="absolute p-8 flex flex-col gap-4 items-center">
+        <li className="flex flex-col items-center"><House fill='yellow' size={40} /><Link href='/' className="text-2xl font-semibold">JPX</Link></li>
+        <li className="flex flex-col items-center"><FileUser fill='yellow' size={40} /><Link href='/cv' className="text-2xl font-semibold">CV</Link></li>
+        <li className="flex flex-col items-center"><Bot fill='yellow' size={40} /><Link href='/ai' className="text-2xl font-semibold">AI</Link></li>
+        <li className="flex flex-col items-center"><Music fill='yellow' size={40} /><Link href='/music' className="text-2xl font-semibold">Music</Link></li>
+        <li><ModeSwitcher text={currentColorMode === 'dark' ? 'Light' : 'Dark'} modeSwitchHandler={modeSwitcherHandler} on={currentColorMode === 'dark'} /></li>
     </ul>
 }
--- components/widgets/mode-switcher.tsx
+++ components/widgets/mode-switcher.tsx
@@ -1,5 +1,7 @@
-export default function ModeSwitcher({ text, modeSwitchHandler, }: { text: string, modeSwitchHandler: () => void }) {
-    return <button className="rounded-md text-2xl font-semibold cursor-pointer" onClick={modeSwitchHandler}>
-        {text}
+import { Moon, Sun } from 'lucide-react';
+
+export default function ModeSwitcher({ text = '', modeSwitchHandler, on }: { text: string, modeSwitchHandler: () => void, on: boolean }) {
+    return <button className="rounded-md text-2xl font-semibold cursor-pointer flex flex-row justify-center" onClick={modeSwitchHandler}>
+        {on ? <Sun fill='yellow' color='yellow' size={40} /> : <Moon fill='yellow' size={40} />}
     </button>
 }
