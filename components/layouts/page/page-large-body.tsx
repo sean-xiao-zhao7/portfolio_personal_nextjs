7c758c3dd3704258c5fc85cb839117b17d5238bb
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-01-30 15:35:59 -0500

    Add content divider

--- components/layouts/page/page-large-body.tsx
+++ components/layouts/page/page-large-body.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
  * Page body for home page and pages with emphasis.
  */
 export default function PageLargeBody({ children }: { children: ReactNode }) {
-    return <div className="justify-center text-lg mt-4 pt-4">
+    return <div className="justify-center text-2xl mt-4 pt-4">
         {children}
     </div>
 }
---
+++ components/widgets/content-divider.tsx
@@ -0,0 +1,8 @@
+/**
+ * Add space and 2 newlines
+ */
+export default function ContentDivider() {
+    return <>
+        <hr className="mt-8 mb-8 text-stone-700 light:text-stone-300" />
+    </>
+}
