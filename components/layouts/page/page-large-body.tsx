a367ffab05dbc75725165f631f847f590c43c1a5
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-02-03 16:21:24 -0500

    Add page paragraph

--- components/layouts/page/page-large-body.tsx
+++ components/layouts/page/page-large-body.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
  * Page body for home page and pages with emphasis.
  */
 export default function PageLargeBody({ children }: { children: ReactNode }) {
-    return <div className="text-xl p-12 font-body font-normal bg-stone-900 light:bg-white">
+    return <div className="text-xl p-10 font-body font-normal bg-stone-900 light:bg-white">
         {children}
     </div>
 }
---
+++ components/layouts/page/page-paragraph.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+export default function PageParagraph({ children }: { children: ReactNode }) {
+    return <p className="p-8 my-8 last:mb-0 rounded-md bg-stone-800 light:bg-stone-50">
+        {children}
+    </p>
+}
