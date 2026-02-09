ecde479bdebc8af579888ecbc8349687c9cdad93
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-02-09 14:59:58 -0500

    Set 10 to paddings in page content areas

--- components/layouts/page/about-section.tsx
+++ components/layouts/page/about-section.tsx
@@ -3,10 +3,10 @@ import ContentDivider from "@/components/widgets/content-divider";
 
 export default function AboutSection({ h2Content, h3Content, children }: { h2Content: string, h3Content: string, children: ReactNode }) {
     return <>
-        <h2>{h2Content}</h2>
+        <h2 className="text-2xl pb-1">{h2Content}</h2>
         <h3>{h3Content}</h3>
         <ContentDivider />
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 text-xl">
             {children}
         </div>
     </>
