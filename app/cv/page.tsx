3c6675fd8f2575244cff472ec005bb05d5f55501
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-02-09 14:33:14 -0500

    Add about contents

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,11 +1,38 @@
 import PageBody from "@/components/layouts/page/page-body";
 import PageMainHeader from "@/components/layouts/page/page-header";
+import PageParagraph from "@/components/layouts/page/page-paragraph";
 
 export default function CVPage() {
     return <>
         <PageMainHeader>About</PageMainHeader>
         <PageBody>
-            I am a Software Engineer.
+            <PageParagraph>
+                <h2>Full Stack Software Engineer - Nodis.io Inc.
+                </h2>
+                <div>
+                    Design and develop robust, user-friendly, stable and secure web and mobile software applications based on complex business requirements, while ensuring maintainability, expandability and reusability.
+                    Design and build frontend components using ReactJS, NextJS, TypeScript, Vanilla JavaScript, HTML5, CSS3, SASS, Tailwind CSS, Material UI libraries, etc. Build backend APIs and services using NodeJS, MongoDB, MySQL, AWS, Google Firebase, etc. Build mobile applications using React Native and Flutter, etc.
+                    Implement and troubleshoot using A.I tools like Cursor, ChatGPT and Midjourney.
+                </div>
+            </PageParagraph>
+            <PageParagraph>
+                <h2>Software Programmer Analyst - University of Toronto Libraries.</h2>
+                <div>
+                    Develop, implement and troubleshoot software applications and integrations based on Java/Spring, PHP/CMS, HTML/CSS, Python, Linux DevOps and others. Interact with university patrons to identify and achieve technical goals with their research projects. Adapt to changing requirements quickly in a positive manner regarding both technical and non-technical aspects. Participate in team plans, including testing and deployment, for all new and legacy features.
+                </div>
+            </PageParagraph>
+            <PageParagraph>
+                <h2>Project Mobile Software Engineer - BlackSheep Inc. - 2025 to 2026.
+                </h2>
+                <div>Run a startup project with a small team to design and build mobile and web applications. Design and build frontend using Flutter, Material UI, ReactJS, NextJS, HTML5/CSS3, SASS. Build backend using Google Firebase, NoSQL, Git.
+                </div>
+            </PageParagraph>
+            <PageParagraph><h2>Project Software Engineer - Raymond Mowla Music - 2022 to 2026.</h2>
+                <div>Analyze business requirements, design and develop new software components and maintain existing ones based on Vanilla TypeScript, HTML5, CSS3, SASS,  Material Design and Git. Develop and maintain CI/CD workflow with GitHub.</div>
+            </PageParagraph>
+            <PageParagraph><h2>Project Software Engineer - Fellowship GTA - 2018 to 2025.</h2>
+                <div>Design and build frontend applications using Vue.js, HTML5, CSS3, SASS and other tools. Deploy using Git. Manage hosting of applications on.</div>
+            </PageParagraph>
         </PageBody>
     </>
 }
