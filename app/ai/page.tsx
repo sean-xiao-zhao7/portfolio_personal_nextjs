3ca654c01f60affe6d40b2fb4fa5e09bd00b23f9
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-02-10 15:43:44 -0500

    Add images to AI

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -7,13 +7,24 @@ export default function AIPage() {
         <PageMainHeader>Gen A.I. - Midjourney</PageMainHeader>
         <PageBody>
             <div className="flex flex-wrap gap-10 justify-center">
-                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770326249/marigold6_rehsep.png'} alt='Marigold 6' />
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755974/rain1_ybd80y.png'} alt='Rain 1' />                
                 <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770326249/supergirlfly3_ctekqg.png'} alt='Super Girl flying 3' />
                 <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770326249/cyberpunk7_ufovag.png'} alt='Cyberpunk 7' />
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755973/angel1_kwebza.png'} alt='Angel 1' />
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755921/jesse0_abv0ij.png'} alt='Jesse 0' />                
                 <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770754359/shinobu_5_h6bvok.png'} alt='Shinobu 5' />
                 <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770754386/dany3_bjt8ff.png'} alt='Dany 3' />
                 <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770754347/iris_5_ffkxng.png'} alt='Iris Out 5' />
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755655/supergirlfly4_rzmdiy.png'} alt='Super Girl flying 4' />
                 <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770754331/kiki_7_nqxw79.png'} alt='Kiki 7' />
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755655/supergirlfly2_gr4enc.png'} alt='Super Girl flying 2' />                
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755653/starwars1_fsntvx.png'} alt='Star Wars 1' />
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755979/serephin1_hflt8h.png'} alt='Serephin 1' />
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755651/supergirl0_k6r6rv.png'} alt='Super Girl 0' />
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755979/rain4_awnun0.png'} alt='Rain 4' />                                
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755888/starwars7_xsoibl.png'} alt='Star Wars 7' />
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770755969/angel2_qy3lcg.png'} alt='Angel 2' />
+                <ImageItem src={'https://res.cloudinary.com/dvayupwmu/image/upload/v1770326249/marigold6_rehsep.png'} alt='Marigold 6' />
             </div>
         </PageBody>
     </>
