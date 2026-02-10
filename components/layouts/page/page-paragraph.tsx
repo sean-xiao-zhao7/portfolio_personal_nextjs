e0c90b3089908df6c6ba9440aed8f71089969e17
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-02-10 18:30:51 -0500

    Add shadow to paragraph

--- components/layouts/page/page-paragraph.tsx
+++ components/layouts/page/page-paragraph.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 export default function PageParagraph({ children }: { children: ReactNode }) {
-    return <div className="p-10 my-10 last:mb-0 first:mt-0 rounded-md bg-stone-800 light:bg-stone-50">
+    return <div className="p-10 my-10 last:mb-0 first:mt-0 rounded-sm shadow-md bg-stone-800 light:bg-stone-50">
         {children}
     </div>
 }
