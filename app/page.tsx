93e384f9e84a808b01ec59c4228b7679bd5d9b45
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-02-12 13:44:50 -0500

    Separate home page into sections

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,20 @@ export default function Home() {
     </PageLargeHeader>
     <PageLargeBody>
       <PageParagraph>
-        <p>Welcome to my homepage!</p>
-        <br />
-        <p>Use the menu on left to explore.</p>
+        <p>Welcome to my home page!</p>
+        <p>Use the menu on the left to explore the site.</p>
+      </PageParagraph>
+      <PageParagraph>
+        <p>C.V. - curriculum vitae: see my list of career projects and history.</p>
+      </PageParagraph>
+      <PageParagraph>
+        <p>A.I. - Check out A.I. images made with Midjourney.</p>
+      </PageParagraph>
+      <PageParagraph>
+        <p>Music - See some music recommendations.</p>
+      </PageParagraph>
+      <PageParagraph>
+        <p>Blog - Read blog posts.</p>
       </PageParagraph>
     </PageLargeBody>
   </>
