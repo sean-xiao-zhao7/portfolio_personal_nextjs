e81b7f03d0ffce164c95933160a7341667e5db2d
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-02-13 15:18:51 -0500

    Finish music single track

--- components/music/single-track-youtube.tsx
+++ components/music/single-track-youtube.tsx
@@ -1,10 +1,10 @@
 import SuspenseLoader from "../widgets/suspense-loader"
 
 export default function SingleTrackYouTube({ title, artist, src }: { title: string, artist: string, src: string }) {
-    return <div className="flex gap-10 justify-between rounded-xl shadow-md bg-stone-800 light:bg-stone-50">
-        <div className="p-10">
-            <h2>{title}</h2>
-            <p>
+    return <div className="flex gap-10 justify-between rounded-xl shadow-md bg-stone-800 light:bg-stone-50 border-1 border-stone-800 light:border-stone-100">
+        <div className="p-10 flex flex-col gap-10 place-content-center place-items-center w-1/3">
+            <h2 className="text-4xl">{title}</h2>
+            <p className="text-2xl">
                 {artist}
             </p>
         </div>
@@ -15,7 +15,7 @@ export default function SingleTrackYouTube({ title, artist, src }: { title: stri
                 referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
                 loading="lazy"
-                className="w-1/2 h-70 rounded-e-xl"
+                className="w-2/3 h-80 rounded-e-xl"
             >
             </iframe>
         </SuspenseLoader>
