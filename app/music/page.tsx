848c91eab573144434bf95329a623e390ca8865d
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-02-13 15:01:18 -0500

    Add song titles

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -6,17 +6,17 @@ export default function MusicPage() {
     return <>
         <PageMainHeader>Music</PageMainHeader>
         <PageBody>
-            <div className="flex flex-col flex-wrap gap-10 place-content-center place-items-center">
-                <SingleTrackYouTube src='https://www.youtube.com/embed/6YZlFdTIdzM' />
-                <SingleTrackYouTube src='https://www.youtube.com/embed/f8aCemLHDdw' />
-                <SingleTrackYouTube src='https://www.youtube.com/embed/V_cHzP8Uexw' />
-                <SingleTrackYouTube src='https://www.youtube.com/embed/RUfGuWeFcQY' />
-                <SingleTrackYouTube src='https://www.youtube.com/embed/d6i4AtCxrDo' />
-                <SingleTrackYouTube src='https://www.youtube.com/embed/AVlaVkH9AQE' />
-                <SingleTrackYouTube src='https://www.youtube.com/embed/n7MyoBki9wk' />
-                <SingleTrackYouTube src='https://www.youtube.com/embed/E7I7nREchHc' />
-                <SingleTrackYouTube src='https://www.youtube.com/embed/b4lYvHE_cvM' />
-                <SingleTrackYouTube src='https://www.youtube.com/embed/uAXIaft7fwI' />
+            <div className="flex flex-col flex-wrap gap-10 place-content-center place-content-stretch">
+                <SingleTrackYouTube src='https://www.youtube.com/embed/6YZlFdTIdzM' title="Clock Strikes" artist="ONE OK ROCK" />
+                <SingleTrackYouTube src='https://www.youtube.com/embed/f8aCemLHDdw' title="Forever" artist="「Story of Hope」" />
+                <SingleTrackYouTube src='https://www.youtube.com/embed/V_cHzP8Uexw' title="「秋明菊」" artist="神はサイコロを振らない" />
+                <SingleTrackYouTube src='https://www.youtube.com/embed/RUfGuWeFcQY' title="「君へ送る唄」" artist="みきなつみ" />
+                <SingleTrackYouTube src='https://www.youtube.com/embed/d6i4AtCxrDo' title="「拝啓、少年よ」" artist="Hump Back" />
+                <SingleTrackYouTube src='https://www.youtube.com/embed/AVlaVkH9AQE' title="『Dear My Boo』" artist="當山みれい" />
+                <SingleTrackYouTube src='https://www.youtube.com/embed/n7MyoBki9wk' title="掌で踊る" artist="そこに鳴る" />
+                <SingleTrackYouTube src='https://www.youtube.com/embed/E7I7nREchHc' title="言葉" artist="SECONDWALL" />
+                <SingleTrackYouTube src='https://www.youtube.com/embed/b4lYvHE_cvM' title="Into The Future" artist="Dizzy Sunfist" />
+                <SingleTrackYouTube src='https://www.youtube.com/embed/uAXIaft7fwI' title="「グッバイライアー」" artist="nolala" />
             </div>
         </PageBody>
     </>
--- components/music/single-track-youtube.tsx
+++ components/music/single-track-youtube.tsx
@@ -1,15 +1,23 @@
 import SuspenseLoader from "../widgets/suspense-loader"
 
-export default function SingleTrackYouTube({ src }: { src: string }) {
-    return <SuspenseLoader>
-        <iframe
-            src={src}
-            title="YouTube video player"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-            loading="lazy"
-            className="w-1/3 h-70"
-        >
-        </iframe>
-    </SuspenseLoader>
+export default function SingleTrackYouTube({ title, artist, src }: { title: string, artist: string, src: string }) {
+    return <div className="flex gap-10 justify-between rounded-xl shadow-md bg-stone-800 light:bg-stone-50">
+        <div className="p-10">
+            <h2>{title}</h2>
+            <p>
+                {artist}
+            </p>
+        </div>
+        <SuspenseLoader>
+            <iframe
+                src={src}
+                title="YouTube video player"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+                loading="lazy"
+                className="w-1/2 h-70 rounded-e-xl"
+            >
+            </iframe>
+        </SuspenseLoader>
+    </div>
 }
