37be7cad9eb892b27a4396ae8919ebec7e818baa
Jean Paul Xiao <xiaofeng.zhao@utoronto.ca>
2026-08-14 13:56:58 -0400

    Fix FW image preview shadow

--- components/layouts/containers/container-mid-shadow.tsx
+++ components/layouts/containers/container-mid-shadow.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 export default function ContainerMidShadow({ children, margin, flex = false }: { children: ReactNode, flex?: boolean, margin?: string }) {
-    return <div className={`relative shadow-xl ${(margin ? margin : '')} ${(flex ? 'flex' : '')}`}>
+    return <div className={`relative shadow-xl ${(margin ? margin : '')} ${(flex ? 'flex' : '')} bg-stone-800 light:bg-stone-50`}>
         {children}
     </div>
 }
